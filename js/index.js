eb37cad394b145b3cca771497ee5a92cf8f7669b
dricaamw <drica.amw@gmail.com>
2024-01-30 12:10:38 -0300

    ouvindo o evento de criar

--- js/index.js
+++ js/index.js
@@ -8,3 +8,27 @@ fecharForm.onclick = () => {
     formCriar.classList.remove('show');
     overlay.classList.remove('show');
 }
+overlay.onclick = () => {
+    formCriar.classList.remove('show');
+    overlay.classList.remove('show');
+}
+formCriar.onsubmit = () => {
+    event.preventDefault();
+
+    // alert(nome.value+' '+capacidade.value);
+    lisTeams.innerHTML = '';
+    lisTeams.innerHTML = `
+    <li>
+        <h4>Nome do Team <box-icon name="show"></box-icon></h4>
+        <h1>0 <span>/ 0</span></h1>
+        <div class="actions">
+        <button>adicionar</button>
+        <button><box-icon type='solid' name='trash-alt'></box-icon></button>
+        </div>
+    </li>
+`;
+
+    formCriar.classList.remove('show');
+    overlay.classList.remove('show');
+}
+
