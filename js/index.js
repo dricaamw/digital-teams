4c0e73e7ed1c6b1f4d2756feb91672100a2fbe16
dricaamw <drica.amw@gmail.com>
2024-02-15 12:05:59 -0300

    update

--- js/index.js
+++ js/index.js
@@ -14,19 +14,19 @@ overlay.onclick = () => {
 }
 formCriar.onsubmit = () => {
     event.preventDefault();
-
+    
     // alert(nome.value+' '+capacidade.value);
-    lisTeams.innerHTML = '';
-    lisTeams.innerHTML = `
-    <li>
-        <h4>${nome.value}<box-icon name="show"></box-icon></h4>
-        <h1>0 <span>/ ${capacidade.value}</span></h1>
-        <div class="actions">
-        <button>adicionar</button>
-        <button><box-icon type='solid' name='trash-alt'></box-icon></button>
-        </div>
-    </li>
-`;
+//     lisTeams.innerHTML = '';
+//     lisTeams.innerHTML = `
+//     <li>
+//         <h4>${nome.value}<box-icon name="show"></box-icon></h4>
+//         <h1>0 <span>/ ${capacidade.value}</span></h1>
+//         <div class="actions">
+//         <button>adicionar</button>
+//         <button><box-icon type='solid' name='trash-alt'></box-icon></button>
+//         </div>
+//     </li>
+// `;
 
     formCriar.classList.remove('show');
     overlay.classList.remove('show');
